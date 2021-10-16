9d43c7fb47361ba2d0880c689256373b1bae7036
Kanchan Jaiswal <91010576+kanchanCode@users.noreply.github.com>
2021-10-16 13:50:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello World " + input);
+            Console.ReadKey();
+
+        }
+    }
+}
